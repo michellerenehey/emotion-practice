9c8a2ba4cbb422421e71a5d408b3e02cffad380e
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-07-13 12:14:40 -0700

    Add styled Cite component with link to source code

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ const Quote = styled("blockquote")((props) => ({
   fontSize: props.size,
 }));
 
+const Cite = styled("cite")(
+  {
+    fontWeight: 100,
+  },
+  (props) => ({
+    fontWeight: props.weight,
+  })
+);
+
 function App() {
   return (
     <div>
@@ -26,6 +35,11 @@ function App() {
         I built this with <code>`emotion/react`</code> and{" "}
         <code>`emotion/styled`</code>!
       </Quote>
+      <Cite weight={700}>
+        <a href="https://www.digitalocean.com/community/tutorials/react-react-emotion">
+          Link to source code!
+        </a>
+      </Cite>
       <div
         css={css({
           margin: 10,
